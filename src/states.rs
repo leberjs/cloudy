6e505b0ee463f3b500145773044c3e01ac855046
Jason Leber <leberjs@gmail.com>
2023-04-28 15:12:10 -0500

    update states with tests; add new state

--- src/states.rs
+++ src/states.rs
@@ -14,16 +14,30 @@ impl Default for AppState {
     }
 }
 
-pub struct ProfileState {
-    pub current: String,
-    pub old: String,
+pub struct ProfileState<'a> {
+    pub current: &'a str,
+    pub old: &'a str,
 }
 
-impl Default for ProfileState {
+impl<'a> Default for ProfileState<'a> {
     fn default() -> Self {
         Self {
-            current: String::from(""),
-            old: String::from(""),
+            current: "",
+            old: "",
+        }
+    }
+}
+
+pub struct LogState<'a> {
+    pub groups: Vec<&'a str>,
+    pub streams: Vec<&'a str>,
+}
+
+impl<'a> Default for LogState<'a> {
+    fn default() -> Self {
+        Self {
+            groups: Vec::new(),
+            streams: Vec::new(),
         }
     }
 }
@@ -33,10 +47,24 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_app_state() {
+    fn test_default_app_state() {
         let app_state = AppState::default();
         assert_eq!(app_state.is_running, false);
         assert_eq!(app_state.show_help, false);
         assert_eq!(app_state.show_profile_selection, false);
+    }
+
+    #[test]
+    fn test_default_profile_state() {
+        let profile_state = ProfileState::default();
+        assert_eq!(profile_state.current, "");
+        assert_eq!(profile_state.old, "");
+    }
+
+    #[test]
+    fn test_default_log_state() {
+        let log_state = LogState::default();
+        assert_eq!(log_state.groups.len(), 0);
+        assert_eq!(log_state.streams.len(), 0);
     }
 }
